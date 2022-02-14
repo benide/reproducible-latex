8141d7b8b3d351e1156f8edd0960e737bcc3d1bf
Ben Ide <ben@bencide.com>
2022-02-14 12:20:18 -0500

    use nixpkgs/nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   description = "LaTeX template for reproducible documents";
 
   inputs = {
-    # nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-21.11;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
     flake-utils.url = github:numtide/flake-utils;
   };
 
